8dfa9849539d114729491138811d77f55109d205
emmeiwhite <emmeiwhite@gmail.com>
2025-11-14 18:45:39 +0530

    8. Code enhancements for real work coding experience

--- src/App.tsx
+++ src/App.tsx
@@ -28,17 +28,27 @@ function App() {
 
   // Let's work on interactivity first
 
-  function handlePrev() {
-    if (step === 1) return
+  // function handlePrev() {
+  //   if (step === 1) return
 
-    setStep(prev => prev - 1) // Component will get re-rendered, App function called which will update the view (UI)
+  //   setStep(prev => prev - 1) // Component will get re-rendered, App function called which will update the view (UI)
+  // }
+
+  function handlePrev() {
+    console.log('handlePrev invoked')
+    setStep(s => Math.max(1, s - 1))
   }
 
   function handleNext() {
-    if (step === 3) return
-
-    setStep(next => next + 1)
+    console.log('handleNext invoked')
+    setStep(s => Math.min(3, s + 1))
   }
+
+  // function handleNext() {
+  //   if (step === 3) return
+
+  //   setStep(next => next + 1)
+  // }
 
   return (
     <section className="h-screen w-full flex justify-center items-center bg-blue-200">
@@ -74,6 +84,7 @@ function App() {
                 <Step
                   step={step}
                   id={stepObj.id}
+                  key={stepObj.id}
                 />
               )
             })}
@@ -109,14 +120,35 @@ function App() {
           </div>
 
           <div className="flex justify-between w-full">
+            {/*  
             <button
               className="px-4 py-2 rounded-3xl cursor-pointer bg-blue-500 text-white hover:bg-blue-600 transition min-w-[100px]"
-              onClick={handlePrev}>
+              onClick={handlePrev}
+              disabled={step === 1}>
               Previous
             </button>
 
             <button
               className="px-4 py-2 rounded-3xl cursor-pointer bg-blue-500 text-white hover:bg-blue-600 transition min-w-[100px]"
+              onClick={handleNext}
+              disabled={step === 3}>
+              Next
+            </button>
+
+            */}
+
+            <button
+              disabled={step === 1}
+              className={`px-4 py-2 rounded-3xl min-w-[100px]
+    ${step === 1 ? 'bg-amber-50 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600 text-white'}`}
+              onClick={handlePrev}>
+              Previous
+            </button>
+
+            <button
+              disabled={step === 3}
+              className={`px-4 py-2 rounded-3xl min-w-[100px]
+    ${step === 3 ? 'bg-amber-50 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600 text-white'}`}
               onClick={handleNext}>
               Next
             </button>
