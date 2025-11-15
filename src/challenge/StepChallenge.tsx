45490298e81430c9b53ff19996d977ee11b6ee44
emmeiwhite <emmeiwhite@gmail.com>
2025-11-15 10:56:04 +0530

    11. State Identified

--- src/challenge/StepChallenge.tsx
+++ src/challenge/StepChallenge.tsx
@@ -1,4 +1,10 @@
+import { useState } from 'react'
+
 export default function StepChallenge() {
+  // After Static UI. Next thing is to identity the state. Values that stay throughout the life-cycle of the component.
+
+  const [count, setCount] = useState(1)
+  const [step, setStep] = useState(1)
   return (
     <main className="flex h-screen justify-center items-center">
       <div className="w-[400px] rounded border p-5 flex flex-col min-h-[200px] justify-between items-center">
@@ -8,7 +14,7 @@ export default function StepChallenge() {
             <button className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer">
               -
             </button>
-            <p className="font-semibold text-xl">Step: 1</p>
+            <p className="font-semibold text-xl">Step: {step}</p>
             <button className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer">
               +
             </button>
@@ -21,7 +27,7 @@ export default function StepChallenge() {
             <button className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer">
               -
             </button>
-            <p className="font-semibold text-xl">Count: 0</p>
+            <p className="font-semibold text-xl">Count: {count}</p>
             <button className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer">
               +
             </button>
@@ -29,7 +35,7 @@ export default function StepChallenge() {
         </div>
 
         {/* Date */}
-        <p>Today is Mon Jun 21 2027</p>
+        <p>Today is {new Date().toUTCString() + count}</p>
       </div>
     </main>
   )
