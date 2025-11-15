0200a2a45265f67316b0774593030b222e993dd2
emmeiwhite <emmeiwhite@gmail.com>
2025-11-15 11:00:04 +0530

    12. Step functionality implemented.

--- src/challenge/StepChallenge.tsx
+++ src/challenge/StepChallenge.tsx
@@ -5,17 +5,37 @@ export default function StepChallenge() {
 
   const [count, setCount] = useState(1)
   const [step, setStep] = useState(1)
+
+  function handleStepPrev() {
+    setStep(prev => prev - 1)
+  }
+
+  function handleStepNext() {
+    setStep(next => next + 1)
+  }
+
+  function handleCountPrev() {
+    setCount(prev => prev - step)
+  }
+
+  function handleCountNext() {
+    setCount(next => next + step)
+  }
   return (
     <main className="flex h-screen justify-center items-center">
       <div className="w-[400px] rounded border p-5 flex flex-col min-h-[200px] justify-between items-center">
         {/* Steps */}
         <div className="w-full">
           <div className="flex rounded-2xl justify-between items-center">
-            <button className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer">
+            <button
+              className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer"
+              onClick={handleStepPrev}>
               -
             </button>
             <p className="font-semibold text-xl">Step: {step}</p>
-            <button className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer">
+            <button
+              className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer"
+              onClick={handleStepNext}>
               +
             </button>
           </div>
@@ -24,11 +44,15 @@ export default function StepChallenge() {
         {/* Count */}
         <div className="w-full">
           <div className="flex rounded-2xl justify-between items-center">
-            <button className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer">
+            <button
+              className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer"
+              onClick={handleCountPrev}>
               -
             </button>
             <p className="font-semibold text-xl">Count: {count}</p>
-            <button className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer">
+            <button
+              className="px-4 py-2 rounded-3xl min-w-20 bg-blue-400 text-white transition-colors hover:bg-blue-500 cursor-pointer"
+              onClick={handleCountNext}>
               +
             </button>
           </div>
